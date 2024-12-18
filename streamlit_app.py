c7caa6b03238965d1c12c2339cd533353379943e
Syakira <140862702+syakiranada@users.noreply.github.com>
2024-12-19 04:30:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,13 @@ st.title('Airplane Passenger Satisfaction Clustering')
 # This is The Header
 st.header("Kelompok 4 Kelas D", divider="green")
 
-# This is The Sub-header
-st.subheader("1. Rania")
-st.subheader("2. Happy")
-st.subheader("3. Syakira")
-st.subheader("4. Syifa")
+with st.expander("**Anggota**"):
+      # Anggota
+      st.markdown("""
+      -  Rania (24060122120013)  
+      -  Happy Desita W (24060122120023)  
+      -  Syakira Nada N (24060122130049)  
+      -  Asy’syifa Shabrina M (24060122130055)  
+        """)
 
 st.write('Hello world!')
