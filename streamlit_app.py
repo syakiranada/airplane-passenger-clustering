6bddcba8c82dac720e7202435b0dd1952ef1e8e2
Syakira <140862702+syakiranada@users.noreply.github.com>
2024-12-19 04:26:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,13 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title('Airplane Passenger Satisfaction Clustering')
+# This is The Header
+st.header("Kelompok 4 Kelas D", divider="green")
+
+# This is The Sub-header
+st.subheader("1. Rania")
+st.subheader("2. Happy")
+st.subheader("3. Syakira")
+st.subheader("4. Syifa")
 
 st.write('Hello world!')
